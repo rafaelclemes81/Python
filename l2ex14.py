c278eb5887542cfffd890ac78708299276778c7b
Rafael Clemes <rclemes1981@gmail.com>
2021-10-06 22:24:05 -0300

    Atualização l2ex14

--- l2ex14.py
+++ l2ex14.py
@@ -10,7 +10,7 @@ e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
 
 O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” se o conceito
 for A, B ou C ou “REPROVADO” se o conceito for D ou E.'''
-
+import os
 
 def cabecalho(c, t):
     print(f'-' * t)
@@ -18,22 +18,38 @@ def cabecalho(c, t):
     print(f'-' * t)
 
 
-def resultado(n, m):
-    for i, v in enumerate(n):
-        print(f'Nota{v}: ')
+def resultado (r, t, nts):
+    soma = 0
+    print(f'-' * t)
+    print(f'{r:^{t}}')
+    print(f'-' * t)
+    for i in range(0, 2):
+        soma += nts[i]
+    media = soma / len(nts)
+    for i, v in enumerate(nts):
+        print(f'NOTA {i+1}: {v:.2f}')
+    print(f'Média Final: {media:.2f}')
+    if 9 <= media <= 10:
+        print(f'Conceito: A'
+              f'\nSituação: Aprovado')
+    elif 7.5 <= media < 9:
+        print(f'Conceito: B'
+              f'\nSituação: Aprovado')
+    elif 6 <= media < 7.5:
+        print(f'Conceito: C'
+              f'\nSituação: Aprovado')
+    elif 4 <= media < 6:
+        print(f'Conceito: D'
+              f'\nSituação: Reprovado')
+    else:
+        print(f'Conceito: E'
+              f'\nSituação: Reprovado')
 
 
 notas = []
-soma = 0
 cabecalho('COLETA DE NOTAS', 30)
 for i in range(0,2):
     notas.append(float(input(f'Informe a {i+1}ª nota: ')))
+resultado('Resultado Final', 30, notas)
 
-for i in range(0,2):
-    soma += notas[i]
-
-media = soma / len(notas)
-
-if 9<= media <=10:
-    cabecalho('Resultado do Aluno', 30)
 
